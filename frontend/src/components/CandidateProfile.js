8b7226483faa59a4870b3a62bda891cee8f53fa5
phucthuan1st <phucthuan1st@gmail.com>
2024-04-12 00:26:01 +0700

    add Profile of Candidate details view
    Signed-off-by: phucthuan1st <phucthuan1st@gmail.com>

--- frontend/src/components/CandidateProfile.js
+++ frontend/src/components/CandidateProfile.js
@@ -1,17 +1,60 @@
-import { React } from "react";
+import React, { useEffect, useState } from "react";
 import { useParams, Link } from "react-router-dom";
-const CandidateProfile = () => {
-  const { candidateId } = useParams();
+import "../styles/CandidateProfile.css";
+import mockData from "../mock/mockCandidateData.json";
 
-  // Add a call to API here to get a candidate profile associated with above id
-  // TODO: implement call to API with suitable model
-  //
-  return (
-    <div className="CandidateProfile">
-      {candidateId}
-      <Link to={".."}>Return</Link>
-    </div>
-  );
+const CandidateProfile = () => {
+    const { candidateId } = useParams();
+    const [candidateData, setCandidateData] = useState(null);
+
+    useEffect(() => {
+        const filteredCandidate = mockData.find(
+            (candidate) => candidate.ProfileId === parseInt(candidateId),
+        );
+        setCandidateData(filteredCandidate);
+    }, [candidateId]);
+
+    if (!candidateData) {
+        return <div className="CandidateProfile">No candidate found.</div>;
+    }
+
+    return (
+        <div className="CandidateProfile">
+            <div className="left">
+                <div className="buttons">
+                    <Link to={".."} className="return-button">
+                        Return
+                    </Link>
+                </div>
+                <img
+                    className="avatar"
+                    src={candidateData.AvatarLink}
+                    alt="Candidate Avatar"
+                />
+            </div>
+            <div className="right">
+                <div>
+                    <h2>{`${candidateData.FirstName} ${candidateData.LastName}`}</h2>
+                    <p>Birthday: {candidateData.Birthday}</p>
+                    <p>Gender: {candidateData.Gender}</p>
+                    <p>Hometown: {candidateData.Hometown}</p>
+                    <p>Status: {candidateData.Status}</p>
+                    <p>Role: {candidateData.RoleName}</p>
+                    <p>Phone Number: {candidateData.PhoneNumber}</p>
+                    <p>Email: {candidateData.Email}</p>
+                    <p>Last Update Date: {candidateData.LastUpdateDate}</p>
+                </div>
+
+                <div className="buttons">
+                    <Link
+                        to={`../update/${candidateId}`}
+                        className="update-button">
+                        Update
+                    </Link>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default CandidateProfile;
