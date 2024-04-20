f47b10e9e7feca328b4b6a7d41d518432f59ac75
grilled-snakehead <snakehead@xmwyw.onmicrosoft.com>
2024-04-20 12:41:03 +0700

    add some chart for testing

--- frontend/src/components/Chart.js
+++ frontend/src/components/Chart.js
@@ -1,9 +1,39 @@
 import React from "react";
+import { Bar, Pie } from "react-chartjs-2";
+import {
+  Chart as ChartJS,
+  ArcElement,
+  Tooltip,
+  Legend,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+} from "chart.js";
+
+ChartJS.register(ArcElement, Tooltip, Legend);
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+);
 
 const Chart = ({ type, data }) => {
-  // Implement logic to render round or column chart based on type and data
+  let chartComponent = null;
 
-  return <div className="Chart">{/* Render chart here */}</div>;
+  // Check the type prop to determine which chart to render
+  if (type === "column") {
+    // Render a column chart
+    chartComponent = <Bar data={data} />;
+  } else if (type === "round") {
+    // Render a round chart
+    chartComponent = <Pie data={data} />;
+  }
+
+  return <div className="Chart">{chartComponent}</div>;
 };
 
 export default Chart;
--- frontend/src/components/Overview.js
+++ frontend/src/components/Overview.js
@@ -1,12 +1,146 @@
 import React from "react";
+import Chart from "./Chart";
+import mockGenderData from "../mock/gender.json";
+import "../styles/Overview.css";
 
 const Overview = () => {
-    return (
-        <div className="Section" id="Overview">
-            <div className="Title">Tổng quan</div>
-            <div className="Content"></div>
+  return (
+    <div className="Section" id="Overview">
+      <div className="Title">Tổng quan</div>
+      <div className="Content">
+        <div className="AllChartContainer">
+          <>
+            <Chart
+              type="round"
+              data={{
+                labels: ["Male", "Female"],
+                datasets: [
+                  {
+                    label: "people",
+                    data: mockGenderData,
+                    backgroundColor: [
+                      "rgba(255, 99, 132, 0.2)",
+                      "rgba(54, 162, 235, 0.2)",
+                    ],
+                    borderColor: [
+                      "rgba(255, 99, 132, 1)",
+                      "rgba(54, 162, 235, 1)",
+                    ],
+                    borderWidth: 1,
+                  },
+                ],
+              }}
+            />
+            <Chart
+              type="round"
+              data={{
+                labels: ["Male", "Female"],
+                datasets: [
+                  {
+                    label: "people",
+                    data: mockGenderData,
+                    backgroundColor: [
+                      "rgba(255, 99, 132, 0.2)",
+                      "rgba(54, 162, 235, 0.2)",
+                    ],
+                    borderColor: [
+                      "rgba(255, 99, 132, 1)",
+                      "rgba(54, 162, 235, 1)",
+                    ],
+                    borderWidth: 1,
+                  },
+                ],
+              }}
+            />
+            <Chart
+              type="round"
+              data={{
+                labels: ["Male", "Female"],
+                datasets: [
+                  {
+                    label: "people",
+                    data: mockGenderData,
+                    backgroundColor: [
+                      "rgba(255, 99, 132, 0.2)",
+                      "rgba(54, 162, 235, 0.2)",
+                    ],
+                    borderColor: [
+                      "rgba(255, 99, 132, 1)",
+                      "rgba(54, 162, 235, 1)",
+                    ],
+                    borderWidth: 1,
+                  },
+                ],
+              }}
+            />
+            <Chart
+              type="round"
+              data={{
+                labels: ["Male", "Female"],
+                datasets: [
+                  {
+                    label: "people",
+                    data: mockGenderData,
+                    backgroundColor: [
+                      "rgba(255, 99, 132, 0.2)",
+                      "rgba(54, 162, 235, 0.2)",
+                    ],
+                    borderColor: [
+                      "rgba(255, 99, 132, 1)",
+                      "rgba(54, 162, 235, 1)",
+                    ],
+                    borderWidth: 1,
+                  },
+                ],
+              }}
+            />
+            <Chart
+              type="round"
+              data={{
+                labels: ["Male", "Female"],
+                datasets: [
+                  {
+                    label: "people",
+                    data: mockGenderData,
+                    backgroundColor: [
+                      "rgba(255, 99, 132, 0.2)",
+                      "rgba(54, 162, 235, 0.2)",
+                    ],
+                    borderColor: [
+                      "rgba(255, 99, 132, 1)",
+                      "rgba(54, 162, 235, 1)",
+                    ],
+                    borderWidth: 1,
+                  },
+                ],
+              }}
+            />
+            <Chart
+              type="round"
+              data={{
+                labels: ["Male", "Female"],
+                datasets: [
+                  {
+                    label: "people",
+                    data: mockGenderData,
+                    backgroundColor: [
+                      "rgba(255, 99, 132, 0.2)",
+                      "rgba(54, 162, 235, 0.2)",
+                    ],
+                    borderColor: [
+                      "rgba(255, 99, 132, 1)",
+                      "rgba(54, 162, 235, 1)",
+                    ],
+                    borderWidth: 1,
+                  },
+                ],
+              }}
+            />
+          </>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Overview;
