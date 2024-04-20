381e0cace9ce1aacd971e413d9078963427666c9
grilled-snakehead <snakehead@xmwyw.onmicrosoft.com>
2024-04-20 11:50:11 +0700

    add chart placeholder

---
+++ frontend/src/components/Chart.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Chart = ({ type, data }) => {
+  // Implement logic to render round or column chart based on type and data
+
+  return <div className="Chart">{/* Render chart here */}</div>;
+};
+
+export default Chart;
